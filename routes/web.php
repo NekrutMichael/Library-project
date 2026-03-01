c21d25629b193e44a54fa28d6166194a5f824c3a
NekrutMichael <ist23-m.nekrut@nubip.edu.ua>
2026-03-01 16:00:52 +0200

    Налаштування маршрутів.

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\Admin\BookController as AdminBookController;
 
 Route::get('/', MainController::class);
 Route::get('/about', AboutController::class);
 Route::get('/books', [BookController::class, 'index']);      
 Route::get('/books/{id}', [BookController::class, 'show']); 
+Route::prefix('admin')->name('admin.')->group(function () {
+Route::resource('books', AdminBookController::class);
+});
