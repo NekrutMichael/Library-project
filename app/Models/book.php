0f17befad330ee72af8e8df5858b871dca350256
NekrutMichael <ist23-m.nekrut@nubip.edu.ua>
2026-03-08 15:14:50 +0200

    Збереження даних у відповідних колонках.

--- app/Models/book.php
+++ app/Models/book.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class book extends Model
 {
     protected $primaryKey = 'BookID';
+    public $timestamps = false; 
+    protected $fillable = [
+        'Title', 'PublicationYear', 'CopiesAvailable', 'CollateralValue', 'DailyRentPrice', 'GenreID'
+    ];
 }
