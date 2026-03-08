b278277ee4d608ce05ac7445d4a4dc543e91c4ee
NekrutMichael <ist23-m.nekrut@nubip.edu.ua>
2026-03-08 15:15:39 +0200

    Додавання методу Create і Store з збереженням та валідацією даних.

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -23,8 +23,24 @@ class BookController extends Controller
         return redirect()->route('admin.books.index')->with('success', 'Книгу успішно видалено!');
     }
 
-    public function create() {}
-    public function store(Request $request) {}
+    public function create() {
+        return view('admin.books.create');
+    }
+    public function store(Request $request) {$validated = $request->validate([
+            'Title' => 'required|string|max:255',
+            'DailyRentPrice' => 'required|numeric|gt:0',
+            'PublicationYear' => 'required|integer',
+            'CopiesAvailable' => 'required|integer|min:0',
+            'CollateralValue' => 'required|numeric|min:0',
+            'GenreID' => 'required|integer'
+        ], [
+            'Title.required' => 'Поле "Назва" є обов\'язковим.',
+            'DailyRentPrice.required' => 'Вкажіть ціну оренди.',
+            'DailyRentPrice.gt' => 'Ціна оренди має бути строго більшою за нуль.',
+        ]);
+        Book::create($validated);
+        return redirect()->route('admin.books.index')
+                         ->with('success', 'Нову книгу успішно додано до каталогу!');}
     public function edit(Book $book) {}
     public function update(Request $request, Book $book) {}
 }
