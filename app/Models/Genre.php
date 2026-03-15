44b5855b874b7cd334dfb9c663149004e3aed3d3
NekrutMichael <ist23-m.nekrut@nubip.edu.ua>
2026-03-15 18:30:43 +0200

    Встановлення Laravel Breeze

---
+++ app/Models/Genre.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Genre extends Model
+{
+    protected $primaryKey = 'GenreID';
+    public $timestamps = false;
+}
